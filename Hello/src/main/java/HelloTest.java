5f4c136b7cbc350640a794eb3194603349e40c12
Alysson Oliveira <3oalysson@gmail.com>
2022-07-05 18:48:48 -0300

    Atividades atualizadas

--- Hello/src/main/java/HelloTest.java
+++
@@ -1,5 +0,0 @@
-public class HelloTest {
-    public static void main (String[] args){
-        System.out.println("Hello TomCat 9");
-    }
-}
--- PrimeiroProjeto/src/Demo.java
+++
@@ -1,7 +0,0 @@
-public class Demo {
-
-    public static void main(String[] args) {
-
-        System.out.println("Teste certo");
-    }
-}
