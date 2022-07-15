56dd8553f44ba4b11736d06c98c392d1455488d1
Alysson Oliveira <3oalysson@gmail.com>
2022-07-15 15:00:19 -0300

    Nova aula

---
+++ Aulas/src/Arquivo.java
@@ -0,0 +1,25 @@
+package Aulas.src;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Arquivo {
+    public static void main(String[] args) {
+        File file = new File("/Users/Alysson/Texto/teste.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e){
+            System.out.println("Error " + e.getMessage());
+        } finally {
+            if (sc != null){
+            sc.close();
+            }
+        }
+    }
+}
