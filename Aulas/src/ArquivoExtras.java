96fc16eb75528c1b76d348daea7134582030fad0
Alysson Oliveira <3oalysson@gmail.com>
2022-07-28 09:35:35 -0300

    Create ArquivoExtras.java

---
+++ Aulas/src/ArquivoExtras.java
@@ -0,0 +1,21 @@
+package Aulas.src;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ArquivoExtras {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com o caminho de um arquivo: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+        
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
+
+        sc.close();
+    }
+}
