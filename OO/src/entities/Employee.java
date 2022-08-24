351fe3c8e2502b744ca906542eaf87713a8d9a72
Alysson Oliveira <3oalysson@gmail.com>
2022-08-24 10:02:41 -0300

    Comparar por salario

--- OO/src/entities/Employee.java
+++ OO/src/entities/Employee.java
@@ -27,6 +27,6 @@ public class Employee implements Comparable<Employee> {
 
     @Override
     public int compareTo(Employee other) {
-        return name.compareTo(other.getName());
+        return salary.compareTo(other.getSalary());
     }
 }
