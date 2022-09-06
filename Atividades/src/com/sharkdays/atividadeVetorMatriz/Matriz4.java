278d171dd0098f68ce217b9caf42e692c62dc315
Alysson Oliveira <3oalysson@gmail.com>
2022-09-06 10:43:43 -0300

    teste

--- Atividades/src/com/sharkdays/atividadeVetorMatriz/Matriz4.java
+++ Atividades/src/com/sharkdays/atividadeVetorMatriz/Matriz4.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class Matriz4 {
     public static void main(String[] args) {
-        String nome = "SENAC";
+
+        String nome = "SENAC TESTE";
         String matriz[][] = new String[3][3];
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matriz[i][j] = nome;
+                if (j == 2){
+                    matriz[i][j] += "\n";
+                }
             }
 
         }
