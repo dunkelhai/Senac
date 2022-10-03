f0ed115cd82ac8108cc89cd954bbaf9cfaa3d603
Alysson Oliveira <3oalysson@gmail.com>
2022-10-03 00:21:43 -0300

    Ajustes

--- GenericSetMap/src/main/java/application/Programa.java
+++ GenericSetMap/src/main/java/application/Programa.java
@@ -1,5 +1,8 @@
 package application;
 
 public class Programa {
-    
+    public static void main(String[] args) {
+
+
+    }
 }
--- GenericSetMap/src/main/java/entities/Pessoa.java
+++ GenericSetMap/src/main/java/entities/Pessoa.java
@@ -6,4 +6,63 @@ public class Pessoa {
     Integer idade;
     Double altura;
     Double peso;
+
+    public Pessoa(Integer id, String nome, Integer idade, Double altura, Double peso) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
 }
