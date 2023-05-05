a971c275f462e78427ed632814f906fb879a6d06
Alysson Oliveira <3oalysson@gmail.com>
2023-05-05 12:07:12 -0300

    Ajustado erros

--- IA/ChatGPT.java
+++ IA/ChatGPT.java
@@ -1,34 +1,37 @@
 package IA;
-import java.net.*;
-import java.io.*;
-import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.google.gson.*;
 
 public class ChatGPT {
-    public static void main(String[] args) throws Exception {
-        String token = "seu_token_de_acesso_aqui";
-        URL url = new URL("https://api.openai.com/v1/engines/davinci-codex/completions");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Authorization", "Bearer " + token);
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setDoOutput(true);
-        String inputJson = "{\"prompt\": \"Qual é a capital do Brasil?\", \"max_tokens\": 1}";
-        OutputStream os = con.getOutputStream();
-        os.write(inputJson.getBytes());
-        os.flush();
-        os.close();
-        int responseCode = con.getResponseCode();
-        System.out.println("Response code: " + responseCode);
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+ public static void main(String[] args) throws Exception {
+            String token = "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";
+            URL url = new URL("https://api.openai.com/v1/engines/davinci-codex/completions");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Authorization", "Bearer " + token);
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setDoOutput(true);
+            String inputJson = "{\"prompt\": \"Qual é a capital do Brasil?\", \"max_tokens\": 1}";
+            OutputStream os = con.getOutputStream();
+            os.write(inputJson.getBytes());
+            os.flush();
+            os.close();
+            int responseCode = con.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            JsonObject responseJson = JsonParser.parseString(content.toString()).getAsJsonObject();
+            String answer = responseJson.getAsJsonArray("choices").get(0).getAsJsonObject().get("text").getAsString();
+            System.out.println("Answer: " + answer);
         }
-        in.close();
-        JSONObject responseJson = new JSONObject(content.toString());
-        String answer = responseJson.getJSONArray("choices").getJSONObject(0).getString("text");
-        System.out.println("Answer: " + answer);
     }
-}
-
