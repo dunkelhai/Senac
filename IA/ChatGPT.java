87d9b84282619ec85a82888789ab3ee68f175adc
Alysson Oliveira <3oalysson@gmail.com>
2023-05-16 00:38:39 -0300

    Ajustes novos

--- IA/ChatGPT.java
+++ IA/ChatGPT.java
@@ -6,7 +6,6 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import com.google.gson.*;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
