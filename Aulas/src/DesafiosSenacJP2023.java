34c2ffe70381c7d022eb39b2928ad29d0e3c387d
Alysson Oliveira <alysson.oliveira@prof.sc.senac.br>
2023-07-03 21:12:58 -0300

    testes

--- Aulas/src/DesafiosSenacJP2023.java
+++ Aulas/src/DesafiosSenacJP2023.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 public class DesafiosSenacJP2023 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int fome, dinheiro;
-        String teste;
-        teste = JOptionPane.showInputDialog("TESTE");
-        if (teste.equals("NOVO")){
-            JOptionPane.showMessageDialog(null, teste);
-        }
-        System.out.println("Está com fome?" +
-                "\n1- Sim" +
-                "\n2- Não");
-        fome = entrada.nextInt();
-        System.out.println("Tem dinheiro?" +
-                "\n1- Sim" +
-                "\n2- Não");
-        dinheiro = entrada.nextInt();
-        if (fome == 1 && dinheiro == 1){
-            System.out.println("Vá para a cantina!");
-        } else if (fome == 1 && dinheiro == 2){
-            System.out.println("Trabalhar que é bom nada.");
-        } else if (fome == 2 && dinheiro == 1){
-            System.out.println("Guarde o dinheiro.");
-        } else if (fome == 2 && dinheiro == 2){
-            System.out.println("Vá caçar o que fazer!");
-        } else {
-            System.out.println("Inválido!");
-        }
+
+//        String nome = JOptionPane.showInputDialog(
+//                "Escreva seu nome:");
+//        JOptionPane.showMessageDialog(null,
+//                "Olá " + nome);
+//        int fome, dinheiro;
+//        System.out.println("Está com fome?" +
+//                "\n1- Sim" +
+//                "\n2- Não");
+//        fome = entrada.nextInt();
+//        System.out.println("Tem dinheiro?" +
+//                "\n1- Sim" +
+//                "\n2- Não");
+//        dinheiro = entrada.nextInt();
+//        if (fome == 1 && dinheiro == 1){
+//            System.out.println("Vá para a cantina!");
+//        } else if (fome == 1 && dinheiro == 2){
+//            System.out.println("Trabalhar que é bom nada.");
+//        } else if (fome == 2 && dinheiro == 1){
+//            System.out.println("Guarde o dinheiro.");
+//        } else if (fome == 2 && dinheiro == 2){
+//            System.out.println("Vá caçar o que fazer!");
+//        } else {
+//            System.out.println("Inválido!");
+//        }
 
     }
 }
