43203c2849c0d46273d4dae41d5d86a51b504250
Alysson Oliveira <alysson.oliveira@prof.sc.senac.br>
2023-07-03 21:20:28 -0300

    ajustes

--- Aulas/src/DesafiosSenacJP2023.java
+++ Aulas/src/DesafiosSenacJP2023.java
@@ -3,7 +3,34 @@ import java.util.Scanner;
 
 public class DesafiosSenacJP2023 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+//        Scanner entrada = new Scanner(System.in);
+
+//        Double valor1, valor2, valor3;
+//        String produto1, produto2, produto3;
+//
+//        System.out.println("Digite o nome do produto 1:");
+//        produto1 = entrada.next();
+//        System.out.println("Digite o nome do produto 2:");
+//        produto2 = entrada.next();
+//        System.out.println("Digite o nome do produto 3:");
+//        produto3 = entrada.next();
+//
+//        System.out.println("Digite o valor do produto 1:");
+//        valor1 = entrada.nextDouble();
+//        System.out.println("Digite o valor do produto 2:");
+//        valor2 = entrada.nextDouble();
+//        System.out.println("Digite o valor do produto 3:");
+//        valor3 = entrada.nextDouble();
+//
+//        if (valor1 < valor2 && valor1 < valor3){
+//            System.out.println("Produto mais barato é o 1 " + produto1);
+//        } else if (valor2 < valor1 && valor2 < valor3){
+//            System.out.println("Produto mais barato é o 2" + produto2);
+//        } else if (valor3 < valor1 && valor3 < valor2){
+//            System.out.println("Produto mais barato é o 3" + produto3);
+//        } else {
+//            System.out.println("Inválido");
+//        }
 
 //        String nome = JOptionPane.showInputDialog(
 //                "Escreva seu nome:");
