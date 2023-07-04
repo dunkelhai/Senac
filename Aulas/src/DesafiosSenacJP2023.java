b309c6f91d12f3127f3c72a9f25853b0d84c7ec2
Alysson Oliveira <alysson.oliveira@prof.sc.senac.br>
2023-07-03 21:44:30 -0300

    ajustes

--- Aulas/src/DesafiosSenacJP2023.java
+++ Aulas/src/DesafiosSenacJP2023.java
@@ -56,6 +56,34 @@ public class DesafiosSenacJP2023 {
 //        } else {
 //            System.out.println("Inválido!");
 //        }
-
+        try {
+            Integer pontos = 0;
+            JOptionPane.showMessageDialog(null,
+                    "Seja bem-vindo ao quiz do Senac!");
+            String nomeJogador = JOptionPane.showInputDialog(
+                    "Digite seu nome:");
+            String quiz1 = JOptionPane.showInputDialog(
+                    "1- O que significa SENAC?" +
+                            "\nA- CENTRO PARA LOUCOS" +
+                            "\nB- SERVIÇO AO CONSUMIDOR" +
+                            "\nC- SENTRO DEDETIZADOR DE PRAGAS" +
+                            "\nD- DIA DA PIZZA" +
+                            "\nE- SERVIÇO NACIONAL DE APRENDIZAGEM COMERCIAL");
+            if (quiz1.equals("E") || quiz1.equals("e")) {
+                JOptionPane.showMessageDialog(null,
+                        "ACERTOU MISERAVI! BOM DIMAIZI! " + nomeJogador);
+                pontos = 1;
+            } else if (quiz1 != null) {
+                JOptionPane.showMessageDialog(null,
+                        "ERROU PANGARÉ! " + nomeJogador);
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "OPÇÃO INVÁLIDA! " + nomeJogador);
+            }
+            JOptionPane.showMessageDialog(null,
+                    "Você acertou " + pontos + " perguntas!");
+        } catch (NullPointerException e){
+            System.out.println("DEU ERRO DE NPE");
+        }
     }
 }
