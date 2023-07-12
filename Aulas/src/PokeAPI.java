20921e1b69e61a16c2d4813092423ad58edf8492
Alysson Oliveira <alysson.oliveira@prof.sc.senac.br>
2023-07-12 20:06:55 -0300

    Melhorias nos testes da API

--- Aulas/src/PokeAPI.java
+++ Aulas/src/PokeAPI.java
@@ -12,16 +12,37 @@ import java.util.Scanner;
 public class PokeAPI {
 
     public static void main(String[] args) {
+        String nomePokemon = null;
+        String idMovimento = null;
+        String idLocal = null;
+        String escolhaAPI1 = null;
+        String escolhaAPI2 = null;
+
         try {
             // Abertura do Scanner
             Scanner sc = new Scanner(System.in);
-            //Escolher o Pokemon
-            String nomePokemon =
-                    JOptionPane.showInputDialog("" +
-                            "Escreva o nome do Pokemón desejado: ");
+            System.out.println("Qual item você deseja consultar" +
+                    "na API do Pokemón?" +
+                    "\n1-Pokemóns" +
+                    "\n2-Movimentos");
+            Integer escolha = sc.nextInt();
+            if (escolha == 1) {
+                //Escolher o Pokemon
+                nomePokemon =
+                        JOptionPane.showInputDialog("" +
+                                "Escreva o nome do Pokemón desejado: ");
+                escolhaAPI2 = nomePokemon;
+                escolhaAPI1 = "pokemon";
+            } else if (escolha == 2){
+                //Escolher o Pokemon
+                idMovimento = JOptionPane.showInputDialog("" +
+                                "Escreva o ID do Movimento desejado: ");
+                escolhaAPI2 = idMovimento;
+                escolhaAPI1 = "move";
+            }
             // URL da API do PokeAPI
-            URL url = new URL("https://pokeapi.co/api/v2/pokemon/" +
-                    nomePokemon);
+            URL url = new URL("https://pokeapi.co/api/v2/" + escolhaAPI1 + "/" +
+                    escolhaAPI2);
             // Abrir conexão HTTP
             HttpURLConnection connection = (HttpURLConnection)
                     url.openConnection();
@@ -43,14 +64,14 @@ public class PokeAPI {
                 ObjectMapper mapper = new ObjectMapper();
                 JsonNode jsonNode = mapper.readTree(response.toString());
 
-                String pokemonName = jsonNode.get("name").asText();
-                int pokemonHeight = jsonNode.get("height").asInt();
-                int pokemonWeight = jsonNode.get("weight").asInt();
-
-
-                System.out.println("Nome: " + pokemonName);
-                System.out.println("Altura: " + pokemonHeight);
-                System.out.println("Peso: " + pokemonWeight);
+//                String pokemonName = jsonNode.get("name").asText();
+//                int pokemonHeight = jsonNode.get("height").asInt();
+//                int pokemonWeight = jsonNode.get("weight").asInt();
+//
+//
+//                System.out.println("Nome: " + pokemonName);
+//                System.out.println("Altura: " + pokemonHeight);
+//                System.out.println("Peso: " + pokemonWeight);
 
                 System.out.println(jsonNode);
 
