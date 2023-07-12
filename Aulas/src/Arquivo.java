38d75bf690fdba3b96c0d10208fce1d06280d16d
Alysson Oliveira <alysson.oliveira@prof.sc.senac.br>
2023-07-12 18:16:37 -0300

    ajustes

--- Aulas/src/Arquivo.java
+++ Aulas/src/Arquivo.java
@@ -8,7 +8,8 @@ public class Arquivo {
     public static void main(String[] args) {
 
         System.out.println("Leitura do arquivo");
-        File file = new File("C:\\Users\\alysson.oliveira\\Documents/teste.txt");
+        File file =
+                new File("C:\\Users\\alysson.oliveira\\Documents/senac.txt");
         Scanner sc = null;
 
         try {
@@ -18,6 +19,9 @@ public class Arquivo {
             }
         } catch (IOException e){
             System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        } catch (NullPointerException e1){
+            e1.printStackTrace();
         } finally {
             if (sc != null){
             sc.close();
