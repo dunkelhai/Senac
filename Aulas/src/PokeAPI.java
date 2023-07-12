a45b458ad16f532ceae5fa8bb7faaf2587e54818
Alysson Oliveira <alysson.oliveira@prof.sc.senac.br>
2023-07-12 18:32:03 -0300

    Ajustes

--- Aulas/src/PokeAPI.java
+++ Aulas/src/PokeAPI.java
@@ -1,15 +1,22 @@
+package Aulas.src;
+
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class PokeAPI {
     public static void main(String[] args) {
         try {
+            // Abertura do Scanner
+            Scanner sc = new Scanner(System.in);
+            //Escolher o Pokemon
+            String nomePokemon = JOptionPane.showInputDialog("Escreva o nome do Pokemón desejado: ");
             // URL da API do PokeAPI
-            URL url = new URL("https://pokeapi.co/api/v2/pokemon/1");
-
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/" + nomePokemon);
             // Abrir conexão HTTP
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
